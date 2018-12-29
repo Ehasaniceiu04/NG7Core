c7f0ccfa5ab517d81660af0fcfcd53c672492f88
Ehasanul Hoque <ehasanul@data-path.net>
2018-12-29 16:18:08 +0600

    first commit

---
+++ Core.NG6.WebUI/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.NG6.WebUI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Test")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "Ehasanul Hoque", "Bulbul Ahmed", "Ripon", });
+        }
+    }
+}
---
+++ Core.NG6.WebUI/Startup.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Core.NG6.WebUI
+{
+  public class Startup
+  {
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.Use(async (context, next) =>
+      {
+        await next();
+        if ((context.Response.StatusCode == 404) && !Path.HasExtension(context.Request.Path.Value)
+        && !context.Request.Path.Value.StartsWith("/api/"))
+        {
+          context.Request.Path = "/index.html";
+          await next();
+        }
+      });
+      app.UseMvc();
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
+
+    }
+  }
+}
